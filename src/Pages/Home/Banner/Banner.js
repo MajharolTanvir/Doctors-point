1b20015afdeb6879d0fa741cf98fa22491472864
Majharul tanvir <mdtanvir6070@gmail.com>
2022-05-10 16:07:11 +0600

    navBar responsive done

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import banner from '../../../assets/images/chair.png'
+import { FiMapPin, FiClock, FiPhoneCall } from "react-icons/fi";
+import './Banner.css'
+
+const Banner = () => {
+    return (
+        <div className='my-14 banner-bg'>
+
+        </div>
+    );
+};
+
+export default Banner;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,23 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  theme: {
+    extend: {},
+  },
   daisyui: {
     themes: [
       {
-        mytheme: {
+        doctortheme: {
           primary: "#0FCFEC",
           secondary: "#19D3AE",
-          accent: "#3A4256",
-          neutral: "#3d4451",
+          third: "#D4D9E3",
+          accent: "#323848",
+          neutral: "#50F8D1",
           "base-100": "#ffffff",
         },
       },
-      "dark",
-      "cupcake",
+      // "dark",
+      // "cupcake",
     ],
   },
   plugins: [require("daisyui")],
